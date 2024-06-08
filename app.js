3eade8c1b57797426e002f45c6eae82ab5e0d62f
upasanagithubrit <103354398+upasanagithubrit@users.noreply.github.com>
2024-06-08 20:07:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const express = require("express");
+const imageRouter = require('./routes/imageRouter.js');
+const authRouter = require("./routes/authRouter.js");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({origin: true}));
+app.use(express.json());
+
+app.use('/api/auth', authRouter);
+
+// app.use((req, res, next) => {
+//     let token;
+//     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+//         token = req.headers.authorization.split(' ')[1];
+//     }
+//     try{
+//         jwt.verify(token, 'my-secret-123');
+//         next();
+//     }
+//     catch(err){
+//         return res.status(401).json({
+//             status: 'fail',
+//             message: 'Login required',
+//         })
+//     }
+// })
+
+app.use('/api/images', imageRouter);
+
+module.exports = app;
+ 
---
+++ server.js
@@ -0,0 +1,18 @@
+// require('dotenv').config()
+process.env.DB_USER='upasnachoudhary216';
+process.env.DB_PASSWORD='Mongo216';
+process.env.DB_NAME='Image-Database';
+const mongoose = require ('mongoose');
+const app = require('./app.js');
+const url = 'mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.csvjhhd.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0';
+let dbLink = url.replace("$_USERNAME_$", process.env.DB_USER);
+dbLink = dbLink.replace("$_PASSWORD_$", process.env.DB_PASSWORD);
+dbLink = dbLink.replace("$_DB_NAME_$", process.env.DB_NAME);
+
+mongoose.connect(dbLink).then(() => {
+  console.log('-------- Database Connected --------');
+});
+
+app.listen(1400,() => {
+    console.log('----------- App Started -----------')
+});
